7bf126e64ca2031f3d57a2b16f1c3674ce33d701
SamJakob <me@samjakob.com>
2023-09-09 01:00:12 +0100

    Fixes #1

--- github-homepage.js
+++ github-homepage.js
@@ -2,7 +2,35 @@
 
 (() => {
 
-    const GITHUB_ISSUES_URL = "https://github.com/SamJakob/github-homepage/issues";
+    // IMPLEMENTATION SETTINGS
+
+    /**
+     * The URL of the dashboard feed (will be resolved relative to the current
+     * page's URL).
+     */
+    const DASHBOARD_FEED_URL = "/dashboard-feed";
+
+    /**
+     * Paths, for which, the old dashboard feed should be eagerly loaded to
+     * reduce loading times.
+     * 
+     * NOTE: Other pages will still load the old dashboard feed if a
+     * .feed-container element is detected, but this will be done after the
+     * element has been detected (i.e. it will not be eagerly loaded).
+     * 
+     * (So honestly, this feature might be overkill but hey - this extension is
+     *  all about that good UX!)
+     */
+    const EAGER_LOAD_PATHS = ["/", "/dashboard"];
+
+    // --------------------------------------------------------------
+
+    // EXTENSION SETTINGS
+
+    /**
+     * A URL for reporting issues with the extension.
+     */
+    const EXT_GITHUB_ISSUES_URL = "https://github.com/SamJakob/github-homepage/issues";
 
     // --------------------------------------------------------------
 
@@ -69,7 +97,7 @@
         let wrapperTextLink = document.createElement('a');
         wrapperTextLink.target = "_blank";
         wrapperTextLink.rel = "nofollow";
-        wrapperTextLink.href = GITHUB_ISSUES_URL;
+        wrapperTextLink.href = EXT_GITHUB_ISSUES_URL;
         wrapperTextSpan = document.createElement('span');
         wrapperTextSpan.textContent = ".";
         wrapperText.appendChild(wrapperTextSpan);
@@ -79,66 +107,129 @@
     }
 
     // --------------------------------------------------------------
-    // Create a MutationObserver to override the feed-container with the
-    // loading animation as soon as it becomes available.
 
-    let _locateElementResolve, _locateElementReject;
-    const locateElementCompleter = new Promise(
-        (resolve, reject) => {
+    /**
+     * Contains and triggers the code necessary to override the feed-container
+     * with the loading animation as soon as it becomes available (and then,
+     * of course, the good feed as soon as it has loaded).
+     * 
+     * @param {boolean} eager Whether or not to eagerly load the old dashboard
+     * feed (i.e. before the feed-container is available). (We can do this
+     * when the path is "/" or "/dashboard", otherwise we should hold off to
+     * avoid slamming the GitHub servers with requests.)
+     */
+    function triggerOverride(eager = false) {
+        
+        // --------------------------------------------------------------
+        // Create a MutationObserver to override the feed-container with the
+        // loading animation as soon as it becomes available.
+
+        let _locateElementResolve;
+        const locateElementCompleter = new Promise((resolve) => {
             _locateElementResolve = resolve;
-            _locateElementReject = reject;
-        }
+        });
+
+        const observer = new MutationObserver((_, observer) => {
+            // Override the feed container with the loading animation to ensure
+            // there is no FOUC whilst the better feed is loaded in to replace it.
+            const feedContainer = document.querySelector('feed-container');
+            if (!feedContainer) return;
+
+            feedContainer.replaceChildren(loader('One moment please...'));
+            _locateElementResolve(feedContainer);
+
+            // Once we've replaced the content, we can stop observing.
+            observer.disconnect();
+        });
+
+        observer.observe(document.documentElement, {
+            childList: true,
+            subtree: true,
+        });
+
+        // --------------------------------------------------------------
+
+        // Wait for the homepage feed loader to complete and then replace the
+        // feedContainer's contents with the result (once the container has
+        // been located).
+        (async () => {
+            // If eager is not specified, we'll wait for the
+            // locateElementCompleter to find the feedContainer first.
+            //
+            // Otherwise, we'll continue which will let the promise resolve
+            // simultaneously as we wait for the container to appear.
+            let feedContainer;
+            if (!eager) feedContainer = await locateElementCompleter;
+
+            // Attempt to fetch the dashboard.
+            (new Promise(async (resolve) => {
+                try {
+                    // Compute the path to the old dashboard homepage.
+                    const goodHomepagePath = new URL(
+                        DASHBOARD_FEED_URL,
+                        window.location.href
+                    ).href;
+
+                    // Fetch and parse the old dashboard homepage.
+                    const goodHomepageReq = await fetch(goodHomepagePath);
+                    const goodHomepageRaw = await (goodHomepageReq).text();
+                    const parser = new DOMParser();
+                    const goodHomepage = parser.parseFromString(
+                        goodHomepageRaw,
+                        "text/html"
+                    );
+
+                    // Extract the feed from the old homepage.
+                    const goodHomepageFeed = goodHomepage.querySelector(
+                        '.application-main > main > div'
+                    );
+
+                    // Replace the new feed with the old one.
+                    resolve(goodHomepageFeed);
+                } catch (ex) {
+                    resolve(error(
+                        "Sorry! Something went wrong whilst loading the old dashboard."
+                    ));
+                }
+            })).then(async (result) => {
+                // If the feedContainer has not been found yet, but the promise
+                // has concluded (i.e., eager was true), we'll wait for the
+                // locateElementCompleter to find the feedContainer.
+                if (!feedContainer) feedContainer = await locateElementCompleter;
+                feedContainer.replaceChildren(result);
+            });
+        })();
+
+    }
+
+    // Immediately trigger the override to load the old dashboard.
+    triggerOverride(
+        // Eagerly load if window.location.pathname is in EAGER_LOAD_PATHS.
+        EAGER_LOAD_PATHS.find((path) => window.location.pathname === path)
     );
 
-    const observer = new MutationObserver((_, observer) => {
-        // Override the feed container with the loading animation to ensure
-        // there is no FOUC whilst the better feed is loaded in to replace it.
-        const feedContainer = document.querySelector('feed-container');
-        if (!feedContainer) return;
+    // Hook into the Turbo pre-render event to override the feed-container
+    // if Turbo is used to navigate to a dashboard page.
+    addEventListener('turbo:before-render', (event) => {
+        // Inspect the body of the page being rendered to see if it contains
+        // a feed-container.
+        const newPage = event.detail.newBody;
+        const newFeedContainer = newPage.querySelector('feed-container');
 
-        feedContainer.replaceChildren(loader('One moment please...'));
-        _locateElementResolve(feedContainer);
-
-        // Once we've replaced the content, we can stop observing.
-        observer.disconnect();
-    });
-
-    observer.observe(document.documentElement, {
-        childList: true,
-        subtree: true,
-    });
-
-    // --------------------------------------------------------------
-    // Then, attempt to fetch the dashboard and replace the feed-container
-    // with it.
-
-    const goodHomepageFeedPromise = new Promise(async (resolve, reject) => {
-        try {
-            // Fetch and parse the old dashboard homepage.
-            const goodHomepageRaw = await (await fetch(`${window.location.href}/dashboard-feed`)).text();
-            const parser = new DOMParser();
-            const goodHomepage = parser.parseFromString(
-                goodHomepageRaw,
-                "text/html"
-            );
-
-            // Extract the feed from the old homepage.
-            const goodHomepageFeed = goodHomepage.querySelector('.application-main > main > div');
-
-            // Replace the new feed with the old one.
-            resolve(goodHomepageFeed);
-        } catch (ex) {
-            resolve(error("Sorry! Something went wrong whilst loading the old dashboard."));
+        // If the new page contains a feed-container, we need to override it.
+        if (newFeedContainer) {
+            // Replace the feed-container with the loading animation.
+            newFeedContainer.replaceChildren(loader('One moment please...'));
+            
+            // Trigger the override.
+            // In this case, we can always eagerly load the old dashboard feed
+            // as we already know that the page contains a feed-container.
+            // (Turbo gave us the opportunity to both detect and inject our
+            //  loading animation into the feed container, so it must be
+            //  there.)
+            triggerOverride(true);
         }
-    });
-
-    // Wait for the homepage feed loader to complete and then replace the
-    // feedContainer's contents with the result (once the container has
-    // been located).
-    goodHomepageFeedPromise.then(async (result) => {
-        const feedContainer = await locateElementCompleter;
-        feedContainer.replaceChildren(result);
-    });
+    })
 
     // --------------------------------------------------------------
 
